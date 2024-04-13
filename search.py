2b281af512b63e3404816da87d25e88f97349c31
Gustavo3211 <68749937+Gustavo3211@users.noreply.github.com>
2024-04-12 23:39:47 -0300

    Make the information more readable
    Add warning for small sample sizes in win rate table
    
    - Added a check to identify rows in the 'gamenumber' table where the total number of matches played is less than 10.
    - Rows with less than 10 total matches are now highlighted in red using the 'table-danger' and 'text-dark' classes.
    - This change helps to indicate when the win rate data might not be reliable due to a small sample size.

--- search.py
+++ search.py
@@ -32,7 +32,6 @@ def replaytable(url):
         df['Win'] = df['Score'].str.contains('WIN').astype(int)  # Convert boolean to int
         df['Loss'] = df['Score'].str.contains('LOSE').astype(int)
 
-
         win_loss_count = df.groupby('Opp. char')[['Win', 'Loss']].sum().reset_index()
         win_loss_count = win_loss_count.sort_values(by=['Win', 'Loss'], ascending=False)  # Sort win_loss_count
 
@@ -51,13 +50,6 @@ def replaytable(url):
 
         return win_loss_count, win_percentages
 
-
-
-
-
-
-"""search the user name"""
-"""it target h1 tags since are the only ones containing the user name"""
 def search(url):
     website = iniciarbs4(url)
     rating_table = website.find("h2", string="Ratings")
@@ -73,20 +65,26 @@ def search(url):
         if names.text != "Wavu Wank":
             result_html += '<div class="container">'
             result_html += '<h2 class="mt-4">URL: ' + url + '</h2>'
-            result_html += '<h2 class="no-capture text-danger">Note: If your link includes a character name at the end, it will display the win rate and related information for that specific character. Otherwise, it will provide information based on the highest rated character.</h2>'
-            result_html += '<h2 class="mt-4">'+ number_data.string +' </h2>'
+            result_html += '<h2 class="no-capture text-danger">Note: 1. If your link includes a character name at the end, it will display the win rate and related information for that specific character. Otherwise, it will provide information based on the highest rated character.</h2>'
+            result_html += '<h2 class="no-capture text-danger">2. Winrate is calculated based on the number of wins divided by the total number of games (wins + losses). If the total number of games is less than 10, the win rate might not be reliable.</h2>'
+            
+            
+
             result_html += '<h3 class="mt-4">Player: ' + names.text.strip() + '</h3>'
+            result_html += '<h2 class="mt-4">'+ number_data.string +' </h2>'
 
             winloses, winrate = replaytable(url) 
 
             result_html += '<div class="row">'
             result_html += '<div class="col-md-6">'
             result_html += '<h2 class="mt-4">Win/loses against characters: </h2>'
-            result_html += winloses.to_html(index=False, classes="table table-bordered table-dark")
+            result_html += '<p>(Characters with less then 10 matches will be in red)<p>'
+            result_html += winloses.to_html(index=False, classes="table table-bordered table-dark gamenumber")
             result_html += '</div>'
 
             result_html += '<div class="col-md-6">'
             result_html += '<h2 class="mt-4">Winrate against characters: </h2>'
+            result_html += '<div class="">Winrate against characters:</div>'
             result_html += winrate.to_html(index=False, classes="table table-bordered table-dark winrate")
             result_html += '</div>'
             result_html += '</div>'
@@ -94,4 +92,5 @@ def search(url):
             result_html += '</div>'
 
     return result_html
+
 
